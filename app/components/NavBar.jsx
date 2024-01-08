d74a8ae013c0cbd93f3a54bfaf8702599ffcba61
mishkamag <m93.maglaperidze@gmail.com>
2024-01-09 00:51:09 +0400

    init: initial nba blogs project

---
+++ app/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Logo from "./nba_PNG16.png";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <Image src={Logo} alt="Nba logo" width={70} quality={100} />
+      <h1>Nba World</h1>
+      <Link href="/">Go Home</Link>
+      <Link href="/tickets">Go Tickets</Link>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ app/tickets/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tickets = () => {
+  return (
+    <main>
+      <h2>tickets page</h2>
+    </main>
+  );
+};
+
+export default Tickets;

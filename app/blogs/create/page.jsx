070cd8d614bf74692c212e5d5c5b54927f9e39f3
mishkamag <m93.maglaperidze@gmail.com>
2024-01-12 20:27:50 +0400

    feat: add create blog form feature

---
+++ app/blogs/create/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CreateForm from "./CreateForm";
+
+const CreateBlogs = () => {
+  return (
+    <main>
+      <h2 className="text-primary text-center">Add new Blog</h2>
+      <CreateForm />
+    </main>
+  );
+};
+
+export default CreateBlogs;

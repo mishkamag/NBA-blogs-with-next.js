d55aaeffac87ccb48693d053dd63debdb6db33e5
mishkamag <m93.maglaperidze@gmail.com>
2024-01-14 22:43:38 +0400

    feat: add user loggin feature

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import NavBar from "../components/NavBar";
 import Link from "next/link";
 
 const AuthLayout = ({ children }) => {
--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,12 +1,30 @@
 "use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 import AuthForm from "../AuthForm";
 
 export default function Login() {
+  const router = useRouter();
+  const [error, setError] = useState("");
+
   const handleSubmit = async (e, email, password) => {
     e.preventDefault();
+    setError("");
 
-    console.log("login", email, password);
+    const supabase = createClientComponentClient();
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) {
+      setError(error.message);
+    }
+    if (!error) {
+      router.push("/");
+    }
   };
 
   return (
@@ -14,6 +32,8 @@ export default function Login() {
       <h2 className="text-center">Login</h2>
 
       <AuthForm handleSubmit={handleSubmit} />
+
+      {error && <div className="error">{error}</div>}
     </main>
   );
 }

47da9b00ac2c0463930aaa88bf9ef922ef34de8a
mishkamag <m93.maglaperidze@gmail.com>
2024-01-14 22:31:09 +0400

    feat: add logout feature

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Logo from "./nba_PNG16.png";
+import LogoutButton from "./LogoutButton";
 
 const NavBar = ({ user }) => {
   return (
@@ -11,8 +12,11 @@ const NavBar = ({ user }) => {
       </Link>
       <h1>Nba World</h1>
       <Link href="/">Go Home</Link>
-      <Link href="/blogs">Go Blogs</Link>
+      <Link href="/blogs" className="mr-auto">
+        Go Blogs
+      </Link>
       {user && <span>Hello {user.email}</span>}
+      <LogoutButton />
     </nav>
   );
 };

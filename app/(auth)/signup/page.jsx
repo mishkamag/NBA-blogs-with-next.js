ed712ff430c21e6e31a3dee5feef797d297b5d5b
mishkamag <m93.maglaperidze@gmail.com>
2024-01-14 21:40:29 +0400

    feat: add email verification with supabase auth

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,12 +1,25 @@
 "use client";
-
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import AuthForm from "../AuthForm";
 
 export default function Signup() {
+  const router = useRouter();
+  const [error, setError] = useState("");
   const handleSubmit = async (e, email, password) => {
     e.preventDefault();
 
-    console.log(email, password);
+    const supabase = createClientComponentClient();
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/api/auth/callback`,
+      },
+    });
+
+    error ? setError(error.message) : router.push("/verify");
   };
 
   return (
@@ -14,6 +27,7 @@ export default function Signup() {
       <h2 className="text-center">Sign up</h2>
 
       <AuthForm handleSubmit={handleSubmit} />
+      {error && <div className="error">{error}</div>}
     </main>
   );
 }
